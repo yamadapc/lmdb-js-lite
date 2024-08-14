be2263db58399fe04017709b28282dee0eb2770e
Pedro Yamada <tacla.yamada@gmail.com>
2024-08-14 02:36:40 +0000

    Update with compatibility layer export

---
+++ compat.js
@@ -0,0 +1,57 @@
+// @ts-check
+const { Lmdb } = require("./index");
+
+/**
+ * @typedef {Object} DBOpenOptions
+ * @property {string} name
+ * @property {string} encoding
+ * @property {boolean} compression
+ */
+
+class LmdbWrapper {
+  /**
+   * @type {Lmdb}
+   */
+  #lmdb;
+
+  /**
+   * @param {Lmdb} lmdb
+   */
+  constructor(lmdb) {
+    this.#lmdb = lmdb;
+  }
+
+  /**
+   * @param {string} key
+   * @returns {Uint8Array}
+   */
+  get(key) {
+    const value = this.#lmdb.getSync(key);
+    return new Uint8Array(value);
+  }
+
+  /**
+   * @param {string} key
+   * @param {Buffer} value
+   */
+  put(key, value) {
+    this.#lmdb.put(key, value);
+  }
+
+  resetReadTxn() {}
+}
+
+/**
+ * @param {string} directory
+ * @param {DBOpenOptions} openOptions
+ * @returns {LmdbWrapper}
+ */
+exports.openDB = function openDB(directory, openOptions) {
+  return new LmdbWrapper(
+    new Lmdb({
+      path: directory,
+      asyncWrites: false,
+      mapSize: 1024 * 1024 * 1024 * 20,
+    }),
+  );
+};
