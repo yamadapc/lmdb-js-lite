16001dfaafda4d62dccca98fb50875fdbc89b465
Pedro Yamada <tacla.yamada@gmail.com>
2024-08-16 15:28:40 +1000

    Split benchmarking sections

---
+++ __benchmarks__/prepare-read-benchmark-unsafe.ts
@@ -0,0 +1,56 @@
+import { randomBytes } from "node:crypto";
+import { mkdirSync, rmSync } from "node:fs";
+import * as v8 from "node:v8";
+import { Lmdb } from "../index";
+import { open as openLMDBUnsafe } from "lmdb";
+
+const ENTRY_SIZE = 64 * 1024; // 64KB
+const ASYNC_WRITES = true;
+const NUM_ENTRIES = Math.floor((1024 * 1024 * 1024 * 5) / ENTRY_SIZE); // Total memory used 1GB
+const MAP_SIZE = 1024 * 1024 * 1024 * 10;
+
+let key = 0;
+
+function generateEntry() {
+  return {
+    key: String(key++),
+    value: randomBytes(ENTRY_SIZE),
+  };
+}
+
+async function main() {
+  rmSync("./databases", {
+    recursive: true,
+    force: true,
+  });
+  mkdirSync("./databases", {
+    recursive: true,
+  });
+
+  const unsafeDB = openLMDBUnsafe({
+    path: "./databases/read",
+    encoding: "binary",
+    compression: true,
+    eventTurnBatching: true,
+  });
+
+  console.log("Generating entries for testing");
+  const entries = [...Array(NUM_ENTRIES)].map(() => {
+    return generateEntry();
+  });
+  console.log("Writing entries");
+  for (let entry of entries) {
+    await unsafeDB.put(entry.key, entry.value);
+  }
+  await unsafeDB.put(
+    "benchmarkInfo",
+    v8.serialize({
+      NUM_ENTRIES,
+    }),
+  );
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
--- __benchmarks__/read-throughput-unsafe.ts
+++ __benchmarks__/read-throughput-unsafe.ts
@@ -1,6 +1,7 @@
 import { randomBytes } from "node:crypto";
 import { mkdirSync, rmSync } from "node:fs";
 import { open as openLMDBUnsafe } from "lmdb";
+import v8 from "node:v8";
 
 const KEY_SIZE = 64;
 const ENTRY_SIZE = 64 * 1024; // 64KB
@@ -8,47 +9,27 @@ const MAX_TIME = 10000;
 const NUM_ENTRIES = Math.floor((1024 * 1024 * 1024) / ENTRY_SIZE); // Total memory used 1GB
 const ENABLE_COMPRESSION = true;
 
-let key = 0;
-function generateEntry() {
-  return {
-    key: String(key++),
-    value: randomBytes(ENTRY_SIZE),
-  };
-}
-
 async function main() {
-  rmSync("./databases", {
-    recursive: true,
-    force: true,
-  });
-  mkdirSync("./databases", {
-    recursive: true,
-  });
   const unsafeDB = openLMDBUnsafe({
-    path: "./databases/unsafe",
+    path: "./databases/read",
     compression: ENABLE_COMPRESSION,
     encoding: "binary",
     eventTurnBatching: true,
   });
 
-  console.log("Generating entries for testing");
-  const entries = [...Array(NUM_ENTRIES)].map(() => {
-    return generateEntry();
-  });
-  console.log("Writing entries");
-  await unsafeDB.transaction(async () => {
-    for (let entry of entries) {
-      await unsafeDB.put(entry.key, entry.value);
-    }
-  });
+  const value = unsafeDB.get("benchmarkInfo");
+  if (!value) throw new Error("Run prepare-read-benchmark.ts");
+  const benchmarkInfo = v8.deserialize(value);
+  console.log(benchmarkInfo);
+  const { NUM_ENTRIES } = benchmarkInfo;
 
   console.log("Reading all entries out");
   {
     const start = Date.now();
     const readEntries = [];
     let i = 0;
-    while (Date.now() - start < MAX_TIME && i < entries.length) {
-      readEntries.push(unsafeDB.get(entries[i].key));
+    while (Date.now() - start < MAX_TIME) {
+      readEntries.push(unsafeDB.get(String(i % NUM_ENTRIES)));
       i += 1;
     }
     const duration = Date.now() - start;
